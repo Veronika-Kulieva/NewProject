9ed095801fc2c85650bcfc978f973bb9679e846b
Nika <gviliya2012@mail.ru>
2023-03-22 00:32:10 +0300

    Gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
-public class Main {
-    public static void main(String[] args) {
 
         int eaters = 5; // сколько людей будут есть
 
